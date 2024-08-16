19320cd6b919be2c72637a0e5ab0d5b58e212ac8
g.gutnik <gleb.g@itechpsp.com>
2024-08-16 20:24:12 +0300

    Refactor

--- imla/src/main/java/dev/serhiiyaremych/imla/ui/ImlaExternalSurface.kt
+++ imla/src/main/java/dev/serhiiyaremych/imla/ui/ImlaExternalSurface.kt
@@ -8,8 +8,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.viewinterop.AndroidView
 import kotlinx.coroutines.CoroutineScope
@@ -18,12 +16,6 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.launch
 
-/**
- * [SurfaceScope] is a scoped environment provided by [AndroidExternalSurface] and
- * [AndroidEmbeddedExternalSurface] to handle [Surface] lifecycle events.
- *
- * @sample androidx.compose.foundation.samples.AndroidExternalSurfaceColors
- */
 public interface SurfaceScope {
     /**
      * Invokes [onChanged] when the surface's geometry (width and height) changes.
@@ -39,27 +31,8 @@ public interface SurfaceScope {
     public fun Surface.onDestroyed(onDestroyed: Surface.() -> Unit)
 }
 
-/**
- * [SurfaceCoroutineScope] is a scoped environment provided by
- * [AndroidExternalSurface] and [AndroidEmbeddedExternalSurface] when a new [Surface] is
- * created. This environment is a coroutine scope that also provides access to
- * a [SurfaceScope] environment which can itself be used to handle other [Surface]
- * lifecycle events.
- *
- * @see SurfaceScope
- * @see AndroidExternalSurfaceScope
- *
- * @sample androidx.compose.foundation.samples.AndroidExternalSurfaceColors
- */
 public interface SurfaceCoroutineScope : SurfaceScope, CoroutineScope
 
-/**
- * [AndroidExternalSurfaceScope] is a scoped environment provided when an
- * [AndroidExternalSurface] or [AndroidEmbeddedExternalSurface] is first initialized.
- * This environment can be used to register a lambda to invoke when a new [Surface]
- * associated with the [AndroidExternalSurface]/[AndroidEmbeddedExternalSurface]
- * is created.
- */
 public interface AndroidExternalSurfaceScope {
     /**
      * Invokes [onSurface] when a new [Surface] is created. The [onSurface] lambda
@@ -74,12 +47,6 @@ public interface AndroidExternalSurfaceScope {
     )
 }
 
-/**
- * Base class for [AndroidExternalSurface] and [AndroidEmbeddedExternalSurface] state. This class
- * provides methods to properly dispatch lifecycle events on [Surface] creation,
- * change, and destruction. Surface creation is treated as a coroutine launch,
- * using the specified [scope] as the parent. This scope must be the main thread scope.
- */
 private abstract class BaseAndroidExternalSurfaceState(val scope: CoroutineScope) :
     AndroidExternalSurfaceScope, SurfaceScope {
 
@@ -175,13 +142,6 @@ private fun rememberAndroidExternalSurfaceState(): AndroidExternalSurfaceState {
     return remember { AndroidExternalSurfaceState(scope) }
 }
 
-/**
- * Defines the z-order of an [AndroidExternalSurface]. When using an
- * [AndroidExternalSurface], a new [Surface] is created and displayed as a separate
- * window layer whose position in the windows layer stack relative to the parent
- * window is decided by its z-order. This class provides constants to set that
- * z-order.
- */
 @JvmInline
 public value class AndroidExternalSurfaceZOrder private constructor(public val zOrder: Int) {
     public companion object {
@@ -203,65 +163,6 @@ public value class AndroidExternalSurfaceZOrder private constructor(public val z
     }
 }
 
-/**
- * Provides a dedicated drawing [Surface] as a separate layer positioned by default
- * behind the window holding the [AndroidExternalSurface] composable. Because
- * [AndroidExternalSurface] uses a separate window layer, graphics composition is handled
- * by the system compositor which can bypass the GPU and provide better performance and
- * power usage characteristics compared to [AndroidEmbeddedExternalSurface]. It is therefore
- * recommended to use [AndroidExternalSurface] over [AndroidEmbeddedExternalSurface] whenever
- * possible.
- *
- * The [Surface] provided can be used to present content that's external to Compose, such as
- * a video stream (from a camera or a media player), OpenGL, Vulkan...The provided [Surface]
- * can be rendered into using a thread different from the main thread.
- *
- * The z-ordering of the surface can be controlled using the [zOrder] parameter:
- *
- * - [AndroidExternalSurfaceZOrder.Behind]: positions the surface behind the window
- * - [AndroidExternalSurfaceZOrder.MediaOverlay]: positions the surface behind the window but
- *   above other [AndroidExternalSurfaceZOrder.Behind] surfaces
- * - [AndroidExternalSurfaceZOrder.OnTop]: positions the surface above the window
- *
- * The drawing surface is opaque by default, which can be controlled with the [isOpaque]
- * parameter. When the surface is transparent, you may need to change the z-order to
- * see something behind the surface.
- *
- * To start rendering, the caller must first acquire the [Surface] when it's created.
- * This is achieved by providing the [onInit] lambda, which allows the caller to
- * register an appropriate [AndroidExternalSurfaceScope.onSurface] callback. The [onInit]
- * lambda can also be used to initialize/cache resources needed once a surface is
- * available.
- *
- * After acquiring a surface, the caller can start rendering into it. Rendering into a
- * surface can be done from any thread.
- *
- * It is recommended to register the [SurfaceScope.onChanged] and [SurfaceScope.onDestroyed]
- * callbacks to properly handle the lifecycle of the surface and react to dimension
- * changes. You must ensure that the rendering thread stops interacting with the surface
- * when the [SurfaceScope.onDestroyed] callback is invoked.
- *
- * If a [surfaceSize] is specified (set to non-[IntSize.Zero]), the surface will use
- * the specified size instead of the layout size of this composable. The surface will
- * be stretched at render time to fit the layout size. This can be used for instance to
- * render at a lower resolution for performance reasons.
- *
- * @param modifier Modifier to be applied to the [AndroidExternalSurface]
- * @param isOpaque Whether the managed surface should be opaque or transparent.
- * @param surfaceSize Sets the surface size independently of the layout size of
- *                    this [AndroidExternalSurface]. If set to [IntSize.Zero], the
- *                    surface size will be equal to the [AndroidExternalSurface]
- *                    layout size.
- * @param zOrder Sets the z-order of the surface relative to its parent window.
- * @param isSecure Control whether the surface view's content should be treated as
- *                 secure, preventing it from appearing in screenshots or from being
- *                 viewed on non-secure displays.
- * @param onInit Lambda invoked on first composition. This lambda can be used to
- *               declare a [AndroidExternalSurfaceScope.onSurface] callback that
- *               will be invoked when a surface is available.
- *
- * @sample androidx.compose.foundation.samples.AndroidExternalSurfaceColors
- */
 @Composable
 public fun ImlaExternalSurface(
     modifier: Modifier = Modifier,
@@ -269,7 +170,6 @@ public fun ImlaExternalSurface(
     surfaceSize: IntSize = IntSize.Zero,
     zOrder: AndroidExternalSurfaceZOrder = AndroidExternalSurfaceZOrder.Behind,
     isSecure: Boolean = false,
-    shape: Shape = RectangleShape,
     onUpdate: SurfaceView.(Surface) -> Unit,
     onInit: AndroidExternalSurfaceScope.() -> Unit
 ) {
