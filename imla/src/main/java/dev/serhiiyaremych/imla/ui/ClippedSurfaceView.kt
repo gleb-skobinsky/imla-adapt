83a2f54032aafc9a90aba333b1cf4e1ee1945e46
g.gutnik <gleb.g@itechpsp.com>
2024-08-16 20:25:45 +0300

    Refactor

--- imla/src/main/java/dev/serhiiyaremych/imla/ui/ClippedSurfaceView.kt
+++ imla/src/main/java/dev/serhiiyaremych/imla/ui/ClippedSurfaceView.kt
@@ -2,13 +2,9 @@ package dev.serhiiyaremych.imla.ui
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Paint
 import android.view.SurfaceView
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.toArgb
-
-private val redColor = Paint().apply { color = Color.Red.toArgb() }
+import androidx.compose.ui.graphics.asAndroidPath
 
 internal class ClippedSurfaceView(
     context: Context
@@ -18,11 +14,7 @@ internal class ClippedSurfaceView(
 
     override fun dispatchDraw(canvas: Canvas) {
         clipPath?.let {
-            println("Canvas size: ${canvas.width} ${canvas.height}")
-            for (segment in it) {
-                println(segment)
-            }
-            canvas.clipRect(0f, 0f, 700f, -100f)
+            canvas.clipPath(it.asAndroidPath())
         }
         super.dispatchDraw(canvas)
     }
